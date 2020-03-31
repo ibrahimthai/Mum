2474778545d84e01301d0152cfb882489ed8a1fe
ithai <ithai@uoguelph.ca>
2020-03-31 19:32:59 -0400

    Created SQL Database for Ingredients

---
+++ app/src/main/java/com/example/mum/DBHelper/DBIngredientsHelper.java
@@ -0,0 +1,66 @@
+package com.example.mum.DBHelper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DBIngredientsHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "myIngredients.db";
+    public static final String TABLE_NAME = "myIngredients";
+    public static final String COLUMN_ID = "ID";
+    public static final String COLUMN_INGREDIENT = "INGREDIENTS";
+
+    // Constructor
+    public DBIngredientsHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table "+ TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, INGREDIENTS TEXT)");
+        System.out.println("TABLE CREATED");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+
+    public boolean addData(String ingredient) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_INGREDIENT, ingredient);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        //if date as inserted incorrectly it will return -1
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Cursor getListContents(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return data;
+    }
+
+
+
+
+
+
+
+
+
+
+
+} // End of class
--- app/src/main/java/com/example/mum/MainActivity.java
+++ app/src/main/java/com/example/mum/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.SparseBooleanArray;
 import android.view.Menu;
@@ -17,6 +18,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.mum.CardViewIngredients.IngredientsCardViewActivity;
+import com.example.mum.DBHelper.DBIngredientsHelper;
 import com.github.clans.fab.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -40,20 +42,24 @@ public class MainActivity extends AppCompatActivity {
     };
 
     SparseBooleanArray sparseBooleanArray ;
-
     Button btnDeleteItems;
     ArrayList<String> mySelectedList = new ArrayList<>();
     int addIngredientFlag = 0;
     CheckBox selectAll;
     FloatingActionButton searchBreakfastBtn;
 
-    ArrayList<String> mySelectedListFromIngredientsList = new ArrayList<>();
+    DBIngredientsHelper myIngredientsDB;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //getIngredients();
+
+        myIngredientsDB = new DBIngredientsHelper(this);
+        getIngredients();
 
         autocomplete = findViewById(R.id.autoCompleteTextView);
         ArrayAdapter<String> autocompleteAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, ListViewItems);
@@ -63,12 +69,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String selectedItem = (String) parent.getItemAtPosition(position);
+
                 // here is your selected item
                 System.out.println("You chose: " + selectedItem);
+
+                //getIngredients();
+
+
+
+
 
                 // If list is empty, add an item without going through duplicate check
                 if (mySelectedList.isEmpty()) {
                     mySelectedList.add(selectedItem);
+                    AddData(selectedItem);
                     autocomplete.setText("");
                 }
                 // If something is in the list, check for duplicates before add
@@ -91,7 +105,8 @@ public class MainActivity extends AppCompatActivity {
                         mySelectedList.add(selectedItem);
                         autocomplete.setText("");
                         addIngredientFlag = 0;
-                        Toast.makeText(getApplicationContext(), "Added " + selectedItem, Toast.LENGTH_SHORT).show();
+                        AddData(selectedItem);
+                        //Toast.makeText(getApplicationContext(), "Added " + selectedItem, Toast.LENGTH_SHORT).show();
                     }
                 }
 
@@ -167,8 +182,6 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 sparseBooleanArray = listview.getCheckedItemPositions();
 
-                String ValueHolder = "" ;
-
                 int i = 0 ;
 
                 while (i < sparseBooleanArray.size()) {
@@ -187,10 +200,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    } // End of OnCreate
 
 
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -209,6 +221,41 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    // Adds data to the SQL database
+    public void AddData(String newEntry) {
+
+        boolean insertData = myIngredientsDB.addData(newEntry);
+
+        if(insertData==true){
+            Toast.makeText(this, "Data Successfully Inserted!", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "Something went wrong :(.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Gets the ingredients from the database and put them in the list
+    public void getIngredients () {
+
+        Cursor cursor = myIngredientsDB.getListContents();
+
+        if (cursor.moveToFirst()) {
+            do {
+                // Create new recipe object and add to it using information from query result
+                // Integer.parseInt(cursor.getString(0))
+                //System.out.println(Integer.parseInt(cursor.getString(0)));
+                System.out.println();
+                System.out.println(cursor.getString(1));
+                mySelectedList.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+
+    }
+
+
 
 
 
